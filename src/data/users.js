eec38646fa955a9bbd9e00c3e846d1c2288b68f4
Olga Sowinska <olga.sowinska@fujitsu.com>
2021-11-05 15:34:08 +0100

    changed values of avarage grade

--- src/data/users.js
+++ src/data/users.js
@@ -7,32 +7,32 @@ export const users = [
     {
         name: "Olga Sowiska",
         attendance: '38%',
-        avarage: '2.3',
+        avarage: '1.3',
     },
     {
         name: "Robert Maklowicz",
         attendance: '38%',
-        avarage: '2.3',
+        avarage: '4.0',
     },
     {
         name: "Krzysztof Krawczyk",
         attendance: '38%',
-        avarage: '2.3',
+        avarage: '2.0',
     },
     {
         name: "Madonna Polska",
         attendance: '38%',
-        avarage: '2.3',
+        avarage: '6.0',
     },
     {
         name: "Bob Marley",
         attendance: '38%',
-        avarage: '2.3',
+        avarage: '3.0',
     },
     {
         name: "Quentin Tarantino",
         attendance: '38%',
-        avarage: '2.3',
+        avarage: '3.3',
     },
 
 ]
