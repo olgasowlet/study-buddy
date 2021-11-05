60f93a7b2fb985cef87a58df699d78417e05bc39
Olga Sowinska <olga.sowinska@fujitsu.com>
2021-11-05 15:33:45 +0100

    import components

--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -2,16 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import Button from "components/atoms/Button/Button";
 import { Wrapper } from "./UsersListItem.styles";
-import Avarage from "components/atoms/Avarage/Avarage";
+import AvarageGrade from "components/atoms/AvarageGrade/AvarageGrade";
+import UserData from "components/atoms/UserData/UserData";
 
 function UsersListItem({ userData: { name, attendance, avarage = "0%" } }) {
   return (
     <Wrapper>
-      <Avarage avarage={avarage}/>
-      <div>
-        <p>{name}</p>
-        <p>{attendance}</p>
-      </div>
+      <AvarageGrade avarage={avarage}/>
+      <UserData name={name} attendance={attendance}/>
       <Button />
     </Wrapper>
   );
