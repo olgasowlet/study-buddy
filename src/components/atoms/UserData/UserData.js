6d2296fe2fc8e74bbcbefad0941adaab583ec147
Olga Sowinska <olga.sowinska@fujitsu.com>
2021-11-12 08:51:50 +0100

    added onClick event

--- src/components/atoms/UserData/UserData.js
+++ src/components/atoms/UserData/UserData.js
@@ -2,13 +2,12 @@ import React from "react";
 import PropTypes from "prop-types";
 import { StyledP, Container } from "./UserData.styles";
 import Button from "../Button/Button";
-
-function UserData({ name, attendance }) {
+function UserData({deleteUser, index, name, attendance }) {
   return (
     <Container>
       <StyledP name>
         {name}
-        <Button />
+        <Button onClick={() => deleteUser(index)}/>
       </StyledP>
       <StyledP attendance>attendace: {attendance}</StyledP>
     </Container>
