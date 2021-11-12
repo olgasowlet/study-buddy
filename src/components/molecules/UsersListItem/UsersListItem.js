47843d2a6847b19fdea64c451b1f4f9034d4a954
Olga Sowinska <olga.sowinska@fujitsu.com>
2021-11-12 08:52:04 +0100

    added new props

--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -1,15 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import Button from "components/atoms/Button/Button";
 import { Wrapper } from "./UsersListItem.styles";
 import AvarageGrade from "components/atoms/AvarageGrade/AvarageGrade";
 import UserData from "components/atoms/UserData/UserData";
-
-function UsersListItem({ userData: { name, attendance, avarage = "0%" } }) {
+function UsersListItem({ deleteUser, index, userData: { name, attendance, avarage = "0%" } }) {
   return (
     <Wrapper>
       <AvarageGrade avarage={avarage}/>
-      <UserData name={name} attendance={attendance}/>
+      <UserData name={name} attendance={attendance} index={index} deleteUser={deleteUser}/>
     </Wrapper>
   );
 }
