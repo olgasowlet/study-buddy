bfbe0ce9d2228f0dfd6f4a7878820d2bcd310780
Olga Sowinska <olga.sowinska@fujitsu.com>
2021-11-12 10:51:30 +0100

    added new colors

--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -8,6 +8,8 @@ export const theme = {
       error: '#CB8581',
       warning: '#E1D888',
       darkGrey: '#737C8E',
+      lightBlue: '#ECEFF7',
+      lightPurple: '#C0C7D6'
     },
     fontSize: {
       l: '17px',
