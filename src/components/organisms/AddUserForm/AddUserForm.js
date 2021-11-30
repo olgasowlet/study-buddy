810c2b7d58503def47aa0335527dcbfedd7fae5c
Olga Sowinska <olga.sowinska@fujitsu.com>
2021-11-30 09:14:31 +0100

    created add user form

---
+++ src/components/organisms/AddUserForm/AddUserForm.js
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { StyledTitle, Wrapper } from "../UsersList/UsersList.styles";
+import FormField from "components/molecules/FormField/FormField";
+import { Button } from "components/atoms/Button/Button.styles";
+
+const AddUserForm = ({handleAdduser, inputsValues, onInputChange}) => {
+  return (
+    <Wrapper as="form" onSubmit={(e) => handleAdduser(e)}>
+      <StyledTitle>Add new student</StyledTitle>
+      <FormField
+        label="Name"
+        name="name"
+        id="name"
+        value={inputsValues.name}
+        onChange={onInputChange}
+      />
+      <FormField
+        label="Attendance"
+        name="attendance"
+        id="attendance"
+        value={inputsValues.attendance}
+        onChange={onInputChange}
+      />
+      <FormField
+        label="Avarage"
+        name="avarage"
+        id="avarage"
+        value={inputsValues.avarage}
+        onChange={onInputChange}
+      />
+      <Button type="submit">Add</Button>
+    </Wrapper>
+  );
+}
+
+AddUserForm.propTypes = {};
+
+export default AddUserForm;
--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -1,64 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { users as data } from "data/users";
+import React from "react";
 import UsersListItem from "components/molecules/UsersListItem/UsersListItem";
 import { Wrapper, StyledList, StyledTitle } from "./UsersList.styles";
-import FormField from "components/molecules/FormField/FormField";
-import { Button } from "components/atoms/Button/Button.styles";
 
-const mockAPI = (success) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (data) {
-        resolve([...data]);
-      } else {
-        reject({ message: "Error" });
-      }
-    }, 2000);
-  });
-};
-
-function UsersList() {
-  const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [name, setName] = useState('');
-
-  const deleteUser = (index) => {
-    const newUsers = [...users.slice(0, index), ...users.slice(index + 1)];
-
-    setUsers(newUsers);
-  };
-
-  useEffect(() => {
-    mockAPI()
-      .then((data) => {
-        setIsLoading(false);
-        setUsers(data);
-      })
-      .catch((err) => console.log(err));
-  }, []);
-
-  const onNameChange = (e) => {
-    setName(e.target.value);
-  }
-
+function UsersList({ users, deleteUser, isLoading }) {
   return (
-    <>
-      <Wrapper>
-        <StyledTitle>Add new student</StyledTitle>
-        <FormField label="Name" name="name" id="name" value={name} onChange={onNameChange}/>
-        <FormField label="Attendance" name="attendance" id="attendance" />
-        <FormField label="Avarage" name="avarage" id="avarage" />
-        <Button>Add</Button>
-      </Wrapper>
-      <Wrapper>
-        <StyledTitle>{isLoading ? "Loading..." : "Students list"}</StyledTitle>
-        <StyledList>
-          {users.map((user, i) => (
-            <UsersListItem userData={user} index={i} deleteUser={deleteUser} />
-          ))}
-        </StyledList>
-      </Wrapper>
-    </>
+    <Wrapper>
+      <StyledTitle>{isLoading ? "Loading..." : "Students list"}</StyledTitle>
+      <StyledList>
+        {users.map((user, i) => (
+          <UsersListItem userData={user} index={i} deleteUser={deleteUser} />
+        ))}
+      </StyledList>
+    </Wrapper>
   );
 }
 
